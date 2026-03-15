05dc4d78dd759ff292f172d6ea9f3485af1af0e6
Nanotech1611 <knandhini1611@gmail.com>
2026-03-15 20:35:35 +0530

    Updated secrets

--- project_scamguard/config.py
+++ project_scamguard/config.py
@@ -1,13 +1,9 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-# Loading env varibles 
-load_dotenv()
+import streamlit as st
 
 # API KEY
-gemini_api_key = os.getenv("GEMINI_API_KEY")
-
+gemini_api_key = st.secrets["GEMINI_API_KEY"]
 
 # Models
 DEFAULT_MODEL = "gemini-2.5-flash"
